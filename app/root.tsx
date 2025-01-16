ac823e3400f8634a1998ffb6c70a96a9d8c9307c
amanshaikh8624 <amanshaikh8624@gmail.com>
2025-01-16 22:57:02 +0530

    no build error should appear

--- app/root.tsx
+++ app/root.tsx
@@ -36,12 +36,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-      <Header />
+        <Header />
         {children}
-        <Footer/>
+        <Footer />
         <ScrollRestoration />
         <Scripts />
-
       </body>
     </html>
   );
